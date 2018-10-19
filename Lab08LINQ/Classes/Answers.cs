4867b12ead0bac1897e0b98c3fbf25951efd4314
nguye <nguyenv2@outlook.com>
2018-10-19 11:18:17 -0700

    remove blanks and duplicates

---
+++ Lab08LINQ/Classes/Answers.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Lab08LINQ.Classes
+{
+  public class Answers
+  {
+    [JsonProperty("type")]
+    public string Type { get; set; }
+
+    [JsonProperty("features")]
+    public Features[] Features { get; set; }
+  }
+
+  public class Features
+  {
+    [JsonProperty("type")]
+    public string TypeTwo { get; set; }
+
+    [JsonProperty("geometry")]
+    public Geometry Geometry { get; set; }
+    [JsonProperty("properties")]
+    public Property Properties { get; set; }
+  }
+  public class Geometry
+  {
+    [JsonProperty("type")]
+    public string TypeThree { get; set; }
+
+    [JsonProperty("coordinates")]
+    public double[] Coordinates { get; set; }
+  }
+  public class Property
+  {
+    [JsonProperty("zip")]
+    public string zip { get; set; }
+
+    [JsonProperty("city")]
+    public string City { get; set; }
+
+    [JsonProperty("state")]
+    public string State { get; set; }
+
+    [JsonProperty("address")]
+    public string Address { get; set; }
+
+    [JsonProperty("borough")]
+    public string Borough { get; set; }
+
+    [JsonProperty("neighborhood")]
+    public string Neigborhood { get; set; }
+
+
+    [JsonProperty("county")]
+    public string Country { get; set; }
+  }
+}
---
+++ Lab08LINQ/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Lab08LINQ.Classes;
+using System.IO;
+using System.Linq;
+
+namespace Lab08LINQ
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Answers myAnswers = GetJSON();
+
+      Console.WriteLine("Question #1: Output all Neighborhoods from the JSON File");
+      Console.WriteLine();
+      List<string> neighborhoods = Neighborhoods(myAnswers);
+      Console.WriteLine();
+      Console.WriteLine("Question #2: Remove all blanks in Neighborhoods");
+      Console.WriteLine();
+      List<string> noBlankNeighborhoods = RemoveNull(neighborhoods);
+      Console.WriteLine();
+      Console.WriteLine("Question #3: Remove all duplicates in Neighborhoods");
+      Console.WriteLine();
+      List<string> noDuplicateNeighborhoods= RemoveDuplicates(noBlankNeighborhoods);
+      Console.WriteLine();
+      Console.ReadKey();
+    }
+
+    static Answers GetJSON()
+    {
+      string path = "../../../../data.json";
+      string text = "";
+
+      using (StreamReader sr = File.OpenText(path))
+      {
+        text = sr.ReadToEnd();
+      }
+      Answers myAnswers = JsonConvert.DeserializeObject<Answers>(text);
+      return myAnswers;
+    }
+
+    static List<string> Neighborhoods(Answers myAnswers)
+    {
+      List<string> neighborhoods = new List<string>();
+      foreach (Features feature in myAnswers.Features)
+      {
+        neighborhoods.Add(feature.Properties.Neigborhood);
+        Console.WriteLine(feature.Properties.Neigborhood);
+      }
+      return neighborhoods;
+    }
+
+    static List<string> RemoveNull(List<string> neighborhoods)
+    {
+      var noBlanks = (from result in neighborhoods
+                      where result != ""
+                      select result).ToList<string>();
+
+      foreach (var item in noBlanks)
+      {
+        Console.WriteLine(item);
+      }
+
+      return noBlanks;
+    }
+
+    static List<string> RemoveDuplicates(List<string> noBlankNeighborhoods)
+    {
+      var noDuplicates = noBlankNeighborhoods.GroupBy(x => x).Select(x => x.First()).ToList<string>();
+
+      foreach (var item in noDuplicates)
+      {
+        Console.WriteLine(item);
+      }
+
+      return noDuplicates;
+    }
+
+    static List<string> AllInOneQuery(List<string> neighborhoods)
+    {
+      var noBlanksOrDuplicates = (from result in neighborhoods
+                                  where result != ""
+                                  select result).ToList<string>();
+
+      foreach (var item in noDuplicates)
+      {
+        Console.WriteLine(item);
+      }
+
+      return noDuplicates;
+    }
+
+  }
+}

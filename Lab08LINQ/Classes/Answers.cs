0cc733aab87256429f8447dd492e47c84e488838
nguye <nguyenv2@outlook.com>
2018-10-19 12:31:29 -0700

    completed

--- Lab08LINQ/Classes/Answers.cs
+++ Lab08LINQ/Classes/Answers.cs
@@ -5,25 +5,38 @@ using Newtonsoft.Json;
 
 namespace Lab08LINQ.Classes
 {
+  /// <summary>
+  /// main object from json data
+  /// contains type and features properties
+  /// </summary>
   public class Answers
   {
     [JsonProperty("type")]
     public string Type { get; set; }
-
+    /// <summary>
+    /// this object property contains an array of objects
+    /// </summary>
     [JsonProperty("features")]
     public Features[] Features { get; set; }
   }
-
+  /// <summary>
+  /// defines the Features objects
+  /// </summary>
   public class Features
   {
     [JsonProperty("type")]
     public string TypeTwo { get; set; }
-
+    /// <summary>
+    /// an object within an object
+    /// </summary>
     [JsonProperty("geometry")]
     public Geometry Geometry { get; set; }
     [JsonProperty("properties")]
     public Property Properties { get; set; }
   }
+  /// <summary>
+  /// defines the Geomtry Property
+  /// </summary>
   public class Geometry
   {
     [JsonProperty("type")]
@@ -32,6 +45,9 @@ namespace Lab08LINQ.Classes
     [JsonProperty("coordinates")]
     public double[] Coordinates { get; set; }
   }
+  /// <summary>
+  /// defines the object property, has neighborhood property in it
+  /// </summary>
   public class Property
   {
     [JsonProperty("zip")]
@@ -51,7 +67,6 @@ namespace Lab08LINQ.Classes
 
     [JsonProperty("neighborhood")]
     public string Neigborhood { get; set; }
-
 
     [JsonProperty("county")]
     public string Country { get; set; }
--- Lab08LINQ/Program.cs
+++ Lab08LINQ/Program.cs
@@ -10,6 +10,10 @@ namespace Lab08LINQ
 {
   class Program
   {
+    /// <summary>
+    /// test the methods and answers the question
+    /// </summary>
+    /// <param name="args"></param>
     static void Main(string[] args)
     {
       Answers myAnswers = GetJSON();
@@ -26,6 +30,13 @@ namespace Lab08LINQ
       Console.WriteLine();
       List<string> noDuplicateNeighborhoods= RemoveDuplicates(noBlankNeighborhoods);
       Console.WriteLine();
+      Console.WriteLine("Question #4: Do #1-3 in one query");
+      Console.WriteLine();
+      List<string> allOneOperation = AllInOneQuery(myAnswers);
+      Console.WriteLine();
+      Console.WriteLine("Question #5: Do #3 without lambda expressions");
+      Console.WriteLine();
+      RedoNoDuplicates(noBlankNeighborhoods);
       Console.ReadKey();
     }
 
@@ -41,18 +52,28 @@ namespace Lab08LINQ
       Answers myAnswers = JsonConvert.DeserializeObject<Answers>(text);
       return myAnswers;
     }
-
+    /// <summary>
+    /// outputs a list of all neighborhoods from JSON object data
+    /// </summary>
+    /// <param name="myAnswers">json source objects</param>
+    /// <returns>list of raw neighborhood outputs</returns>
     static List<string> Neighborhoods(Answers myAnswers)
     {
-      List<string> neighborhoods = new List<string>();
-      foreach (Features feature in myAnswers.Features)
+      var neighborhoods = (from result in myAnswers.Features
+                           select result.Properties.Neigborhood)
+                           .ToList<string>();
+
+      foreach (var item in neighborhoods)
       {
-        neighborhoods.Add(feature.Properties.Neigborhood);
-        Console.WriteLine(feature.Properties.Neigborhood);
+        Console.WriteLine(item);
       }
       return neighborhoods;
     }
-
+    /// <summary>
+    /// removes blanks entries from neighborhoods
+    /// </summary>
+    /// <param name="neighborhoods">list of raw neighborhoods</param>
+    /// <returns>blanks removed from list</returns>
     static List<string> RemoveNull(List<string> neighborhoods)
     {
       var noBlanks = (from result in neighborhoods
@@ -66,7 +87,13 @@ namespace Lab08LINQ
 
       return noBlanks;
     }
-
+    /// <summary>
+    /// from removing blank entries, this method takes it further and
+    /// removes duplicate entries
+    /// uses lambda expressions to filter
+    /// </summary>
+    /// <param name="noBlankNeighborhoods">list of neighborhoods without blanks</param>
+    /// <returns>list of neighborhoods with duplicate removed</returns>
     static List<string> RemoveDuplicates(List<string> noBlankNeighborhoods)
     {
       var noDuplicates = noBlankNeighborhoods.GroupBy(x => x).Select(x => x.First()).ToList<string>();
@@ -78,12 +105,35 @@ namespace Lab08LINQ
 
       return noDuplicates;
     }
-
-    static List<string> AllInOneQuery(List<string> neighborhoods)
+    /// <summary>
+    /// from json object, gets all neighborhoods without blanks or duplicates
+    /// </summary>
+    /// <param name="myAnswers">json data converted in objects</param>
+    /// <returns>list of neighborhoods no duplicates no blanks</returns>
+    static List<string> AllInOneQuery(Answers myAnswers)
     {
-      var noBlanksOrDuplicates = (from result in neighborhoods
-                                  where result != ""
-                                  select result).ToList<string>();
+      var noBlanksOrDuplicates = (from result in myAnswers.Features
+                                  where result.Properties.Neigborhood != ""
+                                  select result.Properties.Neigborhood)
+                                  .Distinct().ToList<string>();
+
+      foreach (var item in noBlanksOrDuplicates)
+      {
+        Console.WriteLine(item);
+      }
+
+      return noBlanksOrDuplicates;
+    }
+    /// <summary>
+    /// removes duplicates using linq expressions
+    /// </summary>
+    /// <param name="noBlanks">list of neighborhoods with no blanks</param>
+    /// <returns>list of neighborhoods with duplicates removed</returns>
+    static List<string> RedoNoDuplicates(List<string> noBlanks)
+    {
+      var noDuplicates = (from result in noBlanks
+                          select result)
+                          .Distinct().ToList<string>();
 
       foreach (var item in noDuplicates)
       {
